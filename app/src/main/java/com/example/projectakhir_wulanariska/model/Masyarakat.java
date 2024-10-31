04e566c17458747655d026e423db3f4d3c62c875
UTS_WulanAriskaDewi <wulanariska21S04@gmail.com>
2024-10-31 17:29:26 +0700

    commit

---
+++ app/src/main/java/com/example/projectakhir_wulanariska/model/Masyarakat.java
@@ -0,0 +1,26 @@
+package com.example.projectakhir_wulanariska.model;
+
+import java.io.Serializable;
+
+public class Masyarakat implements Serializable {
+    private int id;
+    private String nik, nama;
+
+    public int getId() { // untuk mendapatkan id
+        return id;
+    }
+    public void setId(int id) { // untuk membuat i
+        this.id = id;
+    }
+    public String getNIK() { // untuk mendapatkan NIK
+        return nik;
+    }
+    public void setNIK(String nik) { // untuk membuat NIK this.nik = nik;
+    }
+    public String getNama() { // untuk mendapatkan Nama
+        return nama;
+    }
+    public void setNama(String nama) { // untuk membuat Nama
+        this.nama=nama;
+    }
+}
